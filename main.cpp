64dbcf7dd69a0fb902b9e0eecc9a9fcbe46aeabd
vladislav.bobylev <vladislav.a.bobylev@gmail.com>
2019-05-27 22:00:24 +0300

    NEW: all products and some checks

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 
 const int variables_count = 3;
 const int functions_count = 4;
+const int function_prod_count = 11;
 
 const int mask_x1 = 1;
 const int mask_x2 = 2;
@@ -16,6 +17,7 @@ void print_values (const int *values, int count, int columns = 4);
 void make_polynom (int *values, int *polynoms, int count);
 void product_polynoms (int *p1, int *p2, int *res, int *table, int count);
 void product_all_polynoms (int *polynoms, int *res, int count);
+int check_polynoms (int *polynoms, int *products, int products_count, int count);
 
 int main (int argc, char **argv)
 {
@@ -37,12 +39,13 @@ int main (int argc, char **argv)
     print_values(polynom, values_count, function_values_count);
 
     printf ("Product:\n");
-    int res[function_values_count];
-    for (int i = 0; i < function_values_count; i++)
+    int res[function_prod_count * function_values_count];
+    for (int i = 0; i < function_prod_count * function_values_count; i++)
         res[i] = 0;
-    product_polynoms(polynom, polynom + function_values_count, res, function_values_count);
 
-    print_values(res, function_values_count, 1);
+    product_all_polynoms(polynom, res, function_values_count);
+
+    print_values(res, function_values_count * function_prod_count, function_values_count);
 
     return 0;
 }
@@ -72,12 +75,12 @@ int read_file (const char *filename, int *values, int count)
 
 void print_values (const int *values, int count, int columns)
 {
-  for (int i = 1; i <= count; ++i)
-  {
-      printf ("%d | ", values[i - 1]);
-      if (i % columns == 0)
-          printf ("\n");
-  }
+    for (int i = 1; i <= count; ++i)
+    {
+        printf ("%d | ", values[i - 1]);
+        if (i % columns == 0)
+            printf ("\n");
+    }
 }
 
 void make_polynom(int *values, int *polynoms, int count)
@@ -97,19 +100,58 @@ void product_polynoms (int *p1, int *p2, int *res, int count)
 {
     for (int i = 0; i < count; i++)
     {
-      if (p1[i] == 1)
-      {
-          for (int j = 0; j < count; j++)
-              if (p2[j] == 1)
-              {
-                  int value = i | j;
-                  res[value] = 1;
-              }
-      }
+        if (p1[i] == 1)
+        {
+            for (int j = 0; j < count; j++)
+                if (p2[j] == 1)
+                {
+                    int value = i | j;
+                    res[value] = 1;
+                }
+        }
     }
 }
 
 void product_all_polynoms (int *polynoms, int *res, int count)
 {
+    int all_mult = 0;
 
+    for (int i = 0; i < functions_count; i++)
+    {
+        for (int j = i + 1; j < functions_count; j++)
+        {
+            product_polynoms(polynoms + i * count, polynoms + j * count, res + count * all_mult, count);
+            all_mult++;
+        }
+    }
+    for (int i = 0; i < functions_count; i++)
+    {
+        int temp[count];
+        for (int k = 0; k < count; k++)
+            temp[k] = 0;
+        int p_num1 = (i + 1) % functions_count;
+        int p_num2 = (i + 2) % functions_count;
+        int p_num3 = (i + 3) % functions_count;
+
+        product_polynoms(polynoms + p_num1 * count, polynoms + p_num2 * count, temp, count);
+        product_polynoms(polynoms + p_num3 * count, temp, res + count * all_mult, count);
+        all_mult++;
+    }
+    int p1_p2_index = 0;
+    int p3_p4_index = 5;
+    product_polynoms(res + count * p1_p2_index, res + count * p3_p4_index, res + count * all_mult, count);
+}
+
+int check_polynoms (int *polynoms, int *products, int products_count, int count)
+{
+    for (int index = 0; index < functions_count; index++)
+        if (polynoms[index * count + 1 << (functions_count - index)] == 1)
+        {
+            printf ("function is not finy f[%d]", index);
+            return -1;
+        }
+    int product_index = 0;
+    for (int i = 0; i < functions_count; i++)
+        for (int j = i + 1; j < functions_count; j++)
+            if (products[product_index * count + ])
 }
